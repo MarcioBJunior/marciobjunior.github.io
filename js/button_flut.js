6d9971cde13288e091a0301bef67c3da563f9680
MarcioBJunior <mgsbrandaojr@gmail.com>
2019-09-17 22:52:42 -0300

    seventeen

--- js/button_flut.js
+++ js/button_flut.js
@@ -1,4 +1,17 @@
 var button = document.getElementById('flutuante')
+var top = document.getElementById('b');
+var checkbox = document.getElementById('bt_chk');
+
+addEventListener("scroll", alterStyleButton);
+alterStyleButton();
+
+function alterStyleButton(){
+    if(top.scrollY < 250 || checkbox.checked == true){
+        button.style.visibility = "hidden";
+    }else {  
+      button.style.visibility = "visible"; 
+    }
+}
 
 button.onclick = function(){
     document.getElementById('top').click();
--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,16 @@
+var painel = document.getElementsByClassName('painel-menu');
+var painelLink = document.getElementsByTagName('a');
+var checkbox = document.getElementById('bt_chk');
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.mainmenu').addEventListener('click', function() {
+        if(checkbox.checked == true){
 
+            checkbox.checked = false;
+          }else{
+           
+          }
+    })
+  });
+
+ 
