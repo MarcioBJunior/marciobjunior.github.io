713ff217ff23156ebb2e7d5f8eca6acfa9d0adde
Márcio Júnior <mgsbrandaojr@gmail.com>
2020-07-05 14:13:10 -0300

    att

--- js/content.js
+++ js/content.js
@@ -3,6 +3,7 @@ var title2 = document.getElementById('title2');
 var title3 = document.getElementById('title3');
 var title4 = document.getElementById('title4');
 
+
 title1.textContent = "Sobre Mim";
 title2.textContent = "O que eu sei?";
 title3.textContent = "Como posso ser útil?";
