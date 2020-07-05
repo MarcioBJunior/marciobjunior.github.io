8527c10b15c765b28236f420910bb356e74a5a9a
Your Name <you@example.com>
2020-07-05 14:10:20 -0300

    new

---
+++ js/content.js
@@ -0,0 +1,9 @@
+var title1 = document.getElementById('title1');
+var title2 = document.getElementById('title2');
+var title3 = document.getElementById('title3');
+var title4 = document.getElementById('title4');
+
+title1.textContent = "Sobre Mim";
+title2.textContent = "O que eu sei?";
+title3.textContent = "Como posso ser útil?";
+title4.textContent = "Em que já trabalhei?";
